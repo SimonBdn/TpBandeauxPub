6d5d252831f7d1067e4456238345dc5d156c8dcb
simon bodin <simon.bodin@etud.univ-jfc.fr>
2024-10-09 09:07:45 +0200

    MultiThreads

--- src/main/java/bandeau/BandeauVerrouillable.java
+++ src/main/java/bandeau/BandeauVerrouillable.java
@@ -3,6 +3,19 @@ package bandeau;
 public class BandeauVerrouillable extends Bandeau implements Runnable {
     @Override
     public void run() {
-        
+        synchronized (this) {
+            String message = "Démonstration du bandeau";
+
+            Scenario s = new Scenario();
+            s.addEffect(new RandomEffect(message, 700), 1);
+            s.addEffect(new TeleType("Je m'affiche caractère par caractère", 100), 1);
+            s.addEffect(new Blink("Je clignote 10x", 100), 10);
+            s.addEffect(new Zoom("Je zoome", 50), 1);
+            s.addEffect(new FontEnumerator(10), 1);
+            s.addEffect(new Rainbow("Comme c'est joli !", 30), 1);
+            s.addEffect(new Rotate("2 tours à droite", 180, 400, true), 2);
+
+            s.playOn(this);
+        }
     }
 }
